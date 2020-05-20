d8273adbd418cd593e1f52078156ced883be9b32
gauthampkrishnan <gauthamkrishnanp@gmail.com>
2020-05-20 09:27:20 +0530

    Random Dice Roller using flutter.

---
+++ android/app/src/main/kotlin/com/example/DiceRoller_App/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.DiceRoller_App
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+
+// Update pubspec file for image asset
+
+void main(){
+  runApp(MyApp());
+
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dice Roller',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home:HomePage(),
+    );
+  }
+}
+
+
